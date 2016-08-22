165939dc1890c1fcb1a70560ddf11007503f537b
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-08-22 23:27:51 +1000

    Added a functionality to write multiple rows at once

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 
 # CSV Writer
 
+## Usage
+
 ```js
 var CsvWriter = require('csv-writer');
 var writer = new CsvWriter({
@@ -18,6 +20,53 @@ var data = [
 
 data.reduce(function (promise, person) {
     return writer.write(person);
+}, Promise.resolve());
+
+// This will produce a file path/to/file.csv with following contents:
+//
+//   NAME,LANGUAGE
+//   Ryuichi,"Japanese, English"
+//   Michael,English
+```
+
+### Write multiple records at once
+
+```js
+var data = [
+    {name: 'Ryuichi', lang: 'Japanese, English'},
+    {name: 'Michael', lang: 'English'}
+];
+
+writer.writeRecords(data);     // return a promise
+
+// This will produce a file path/to/file.csv with following contents:
+//
+//   NAME,LANGUAGE
+//   Ryuichi,"Japanese, English"
+//   Michael,English
+```
+
+## Future plans
+
+### Write array
+
+```js
+var CsvWriter = require('csv-writer');
+var writer = new CsvWriter({
+    path: 'path/to/file.csv',
+    header: [
+        {id: 'name', title: 'NAME'},
+        {id: 'lang', title: 'LANGUAGE'},
+    ]
+});
+
+var data = [
+    ['Ryuichi', 'Japanese, English'],
+    ['Michael', 'English']
+];
+
+data.reduce(function (promise, record) {
+    return writer.write(record);
 }, Promise.resolve());
 
 // This will produce a file path/to/file.csv with following contents:
--- test/lib/csv-writer.test.js
+++ test/lib/csv-writer.test.js
@@ -65,63 +65,6 @@ describe('CsvWriter', () => {
         });
     });
 
-    it('stringifies header values', () => {
-        const fs = {
-            writeFile: sinon.stub().callsArgWith(3, null)
-        };
-        const fieldStringifier = {stringify: value => String(value)};
-        const header = [
-            {id: 'FIELD_A', title: 3},
-            {id: 'FIELD_B', title: 'TITLE_B'}
-        ];
-        const writer = new CsvWriter({
-            fs,
-            fieldStringifier,
-            path: 'FILE_PATH',
-            header
-        });
-
-        return writer.writeHeader().then(() => {
-            expect(fs.writeFile.args[0].slice(0, 3)).to.eql([
-                'FILE_PATH',
-                '3,TITLE_B\n',
-                {encoding: 'utf8'}
-            ]);
-        });
-    });
-
-    it('stringifies field values', () => {
-        const fs = {
-            writeFile: sinon.stub().callsArgWith(3, null)
-        };
-        const fieldStringifier = {stringify: value => String(value)};
-        const header = [
-            {id: 'FIELD_A', title: 'TITLE_A'},
-            {id: 'FIELD_B', title: 'TITLE_B'}
-        ];
-        const writer = new CsvWriter({
-            fs,
-            fieldStringifier,
-            path: 'FILE_PATH',
-            header
-        });
-
-        const row = {
-            FIELD_A: 3,
-            FIELD_B: 'VALUE_B1'
-        };
-        return writer.write(row).then(() => {
-            expect(fs.writeFile.args[1].slice(0, 3)).to.eql([
-                'FILE_PATH',
-                '3,VALUE_B1\n',
-                {
-                    encoding: 'utf8',
-                    flag: 'a'
-                }
-            ]);
-        });
-    });
-
     it('writes no header row if it is not given', () => {
         const fs = {
             writeFile: sinon.stub().callsArgWith(3, null)
@@ -143,6 +86,46 @@ describe('CsvWriter', () => {
             expect(fs.writeFile.args[0].slice(0, 3)).to.eql([
                 'FILE_PATH',
                 'VALUE_A1,VALUE_B1\n',
+                {
+                    encoding: 'utf8',
+                    flag: 'a'
+                }
+            ]);
+        });
+    });
+
+    it('writes multiple records at once', () => {
+        const fs = {
+            writeFile: sinon.stub().callsArgWith(3, null)
+        };
+        const fieldStringifier = {stringify: string => string};
+        const header = [
+            {id: 'FIELD_A', title: 'TITLE_A'},
+            {id: 'FIELD_B', title: 'TITLE_B'}
+        ];
+        const writer = new CsvWriter({
+            fs,
+            fieldStringifier,
+            path: 'FILE_PATH',
+            header
+        });
+
+        const records = [{
+            FIELD_A: 'VALUE_A1',
+            FIELD_B: 'VALUE_B1'
+        }, {
+            FIELD_A: 'VALUE_A2',
+            FIELD_B: 'VALUE_B2'
+        }];
+        return writer.writeRecords(records).then(() => {
+            expect(fs.writeFile.args[0].slice(0, 3)).to.eql([
+                'FILE_PATH',
+                'TITLE_A,TITLE_B\n',
+                {encoding: 'utf8'}
+            ]);
+            expect(fs.writeFile.args[1].slice(0, 3)).to.eql([
+                'FILE_PATH',
+                'VALUE_A1,VALUE_B1\nVALUE_A2,VALUE_B2\n',
                 {
                     encoding: 'utf8',
                     flag: 'a'
