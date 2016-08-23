e6b0068f15e47b8186eb022fa027d366cb717d00
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-08-23 22:13:52 +1000

    Revised test cases

--- test/lib/csv-writer.test.js
+++ test/lib/csv-writer.test.js
@@ -19,11 +19,14 @@ describe('CsvWriter', () => {
             header
         });
 
-        const records = [{FIELD_A: 'VALUE_A1', FIELD_B: 'VALUE_B1'}];
+        const records = [
+            {FIELD_A: 'VALUE_A1', FIELD_B: 'VALUE_B1'},
+            {FIELD_A: 'VALUE_A2', FIELD_B: 'VALUE_B2'}
+        ];
         return writer.writeRecords(records).then(() => {
             expect(fs.writeFile.args[0].slice(0, 3)).to.eql([
                 'FILE_PATH',
-                'TITLE_A,TITLE_B\nVALUE_A1,VALUE_B1\n',
+                'TITLE_A,TITLE_B\nVALUE_A1,VALUE_B1\nVALUE_A2,VALUE_B2\n',
                 {
                     encoding: 'utf8',
                     flag: 'w'
@@ -65,7 +68,7 @@ describe('CsvWriter', () => {
             });
     });
 
-    it('writes no header row if it is not given', () => {
+    it('does not write a header row if the first element of the header is not an object', () => {
         const fs = {
             writeFile: sinon.stub().callsArgWith(3, null)
         };
@@ -83,38 +86,6 @@ describe('CsvWriter', () => {
             expect(fs.writeFile.args[0].slice(0, 3)).to.eql([
                 'FILE_PATH',
                 'VALUE_A1,VALUE_B1\n',
-                {
-                    encoding: 'utf8',
-                    flag: 'w'
-                }
-            ]);
-        });
-    });
-
-    it('writes multiple records at once', () => {
-        const fs = {
-            writeFile: sinon.stub().callsArgWith(3, null)
-        };
-        const fieldStringifier = {stringify: string => string};
-        const header = [
-            {id: 'FIELD_A', title: 'TITLE_A'},
-            {id: 'FIELD_B', title: 'TITLE_B'}
-        ];
-        const writer = new CsvWriter({
-            fs,
-            fieldStringifier,
-            path: 'FILE_PATH',
-            header
-        });
-
-        const records = [
-            {FIELD_A: 'VALUE_A1', FIELD_B: 'VALUE_B1'},
-            {FIELD_A: 'VALUE_A2', FIELD_B: 'VALUE_B2'}
-        ];
-        return writer.writeRecords(records).then(() => {
-            expect(fs.writeFile.args[0].slice(0, 3)).to.eql([
-                'FILE_PATH',
-                'TITLE_A,TITLE_B\nVALUE_A1,VALUE_B1\nVALUE_A2,VALUE_B2\n',
                 {
                     encoding: 'utf8',
                     flag: 'w'
