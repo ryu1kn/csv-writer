83d1bac64755a11453ad1f837cca75148c4e58cb
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-12-04 20:37:28 +1100

    Updated dependency versions
    Updated CodeClimate & eslint config

--- test/lib/csv-writer.test.js
+++ test/lib/csv-writer.test.js
@@ -119,7 +119,7 @@ describe('CsvWriter', () => {
         return writer.writeRecords('RECORDS').then(
             () => new Error('Should have been failed'),
             e => {
-                expect(e).to.be.an.error;
+                expect(e).to.be.an('error');
                 expect(e.message).to.eql('WRITE_FILE_ERROR');
             }
         );
