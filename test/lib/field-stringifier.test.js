439faef73be67799cc01f3aafc9a55899770ddbf
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-03-03 23:34:42 +1100

    Removed repetitive declarations of FieldStringifier

--- test/lib/field-stringifier.test.js
+++ test/lib/field-stringifier.test.js
@@ -2,54 +2,45 @@
 const FieldStringifier = require('../../lib/field-stringifier');
 
 describe('FieldStringifier', () => {
+    const stringifier = new FieldStringifier();
 
     it('returns the same string', () => {
-        const stringifier = new FieldStringifier();
         expect(stringifier.stringify('VALUE')).to.eql('VALUE');
     });
 
     it('preserves the whitespace characters', () => {
-        const stringifier = new FieldStringifier();
         expect(stringifier.stringify(' VALUE\tA  ')).to.eql(' VALUE\tA  ');
     });
 
     it('wraps a field value with double quotes if the field contains comma', () => {
-        const stringifier = new FieldStringifier();
         expect(stringifier.stringify('VALUE,A')).to.eql('"VALUE,A"');
     });
 
     it('wraps a field value with double quotes if the field contains newline', () => {
-        const stringifier = new FieldStringifier();
         expect(stringifier.stringify('VALUE\nA')).to.eql('"VALUE\nA"');
     });
 
     it('wraps a field value with double quotes and escape the double quotes if they are used in the field', () => {
-        const stringifier = new FieldStringifier();
         expect(stringifier.stringify('VALUE"A')).to.eql('"VALUE""A"');
     });
 
     it('escapes double quotes even if double quotes are only on the both edges of the field', () => {
-        const stringifier = new FieldStringifier();
         expect(stringifier.stringify('"VALUE"')).to.eql('"""VALUE"""');
     });
 
     it('converts a number into a string', () => {
-        const stringifier = new FieldStringifier();
         expect(stringifier.stringify(1)).to.eql('1');
     });
 
     it('converts undefined into an empty string', () => {
-        const stringifier = new FieldStringifier();
         expect(stringifier.stringify()).to.eql('');
     });
 
     it('converts null into an empty string', () => {
-        const stringifier = new FieldStringifier();
         expect(stringifier.stringify(null)).to.eql('');
     });
 
     it('converts an object into toString-ed value', () => {
-        const stringifier = new FieldStringifier();
         const obj = {
             name: 'OBJECT_NAME',
             toString: function () { return `Name: ${this.name}`; }
@@ -58,7 +49,6 @@ describe('FieldStringifier', () => {
     });
 
     it('wraps a toString-ed field value with double quote if the value contains comma', () => {
-        const stringifier = new FieldStringifier();
         const obj = {
             name: 'OBJECT,NAME',
             toString: function () { return `Name: ${this.name}`; }
@@ -67,7 +57,6 @@ describe('FieldStringifier', () => {
     });
 
     it('escapes double quotes in a toString-ed field value if the value has double quotes', () => {
-        const stringifier = new FieldStringifier();
         const obj = {
             name: 'OBJECT_NAME"',
             toString: function () { return `Name: ${this.name}`; }
