0d8a35fa00e3492813cdd624fc75d46c580ce8d8
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-08-11 13:35:05 +1000

    Rename test temp file directory

--- test/helper.js
+++ test/helper.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const assert = require('assert');
 
-exports.testFilePath = id => `./test-files/${id}`;
+exports.testFilePath = id => `./test-tmp/${id}.csv`;
 
 exports.assertFile = (path, expectedContents, encoding) => {
     const actualContents = fs.readFileSync(path, encoding || 'utf8');
